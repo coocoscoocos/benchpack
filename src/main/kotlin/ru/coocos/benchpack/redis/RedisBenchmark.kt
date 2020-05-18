29f7c9f6a149d1f94c941e296922e4250ceb51d4
Александр Безруков <a.bezrukov@seuslab.ru>
2020-05-19 00:40:29 +0300

    Run multiple Redis instances with benchmarks (finish)

--- src/main/kotlin/ru/coocos/benchpack/redis/RedisBenchmark.kt
+++ src/main/kotlin/ru/coocos/benchpack/redis/RedisBenchmark.kt
@@ -4,23 +4,40 @@ import java.io.File
 
 object RedisBenchmark {
 
+    val dir = "benchmark"
+    val version = "6.0.3"
+    val filename = "redis-$version.tar.gz"
+    val cpuCount = Runtime.getRuntime().availableProcessors()
+    val startPort = 8000
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val threads = Runtime.getRuntime().availableProcessors()
-//        val mkdirCode = ProcessBuilder("mkdir", "benchmark").start().waitFor()
-//        val wgetRedis = ProcessBuilder("wget", "http://download.redis.io/releases/redis-6.0.3.tar.gz").directory(File("benchmark")).start().waitFor()
-//        val unpackRedis = ProcessBuilder("tar", "-xf", "redis-6.0.3.tar.gz").directory(File("benchmark")).start().waitFor()
-//        val makeRedis = ProcessBuilder("make", "-j", threads.toString()).directory(File("benchmark/redis-6.0.3")).start().waitFor()
-
-        //todo NUMA
-        val serverProcessList = (1..(threads / 2)).map {
-            val port = 8000 + it
-            ProcessBuilder("./redis-server", "--port", port.toString()).directory(File("benchmark/redis-6.0.3/src")).start()
+        val threads = System.getProperty("threads")?.toInt() ?: cpuCount / 2
+        val isNuma = System.getProperty("numa")?.equals("true") ?: false
+        println("Using $threads threads")
+        if (isNuma) {
+            println("NUMA enabled")
+        } else {
+            println("NUMA disabled")
+        }
+        prepare()
+        println("Run")
+        val serverProcessList = (1..threads).map {
+            val port = startPort + it
+            val core = it * 2 - 2
+            val redisServerList = listOf("./redis-server", "--port", port.toString(), "--maxmemory", "2048mb")
+            val tasksetList = listOf("taskset", "-c", core.toString())
+            val cmd = if (isNuma) tasksetList + redisServerList else redisServerList
+            ProcessBuilder(cmd).directory(File("benchmark/redis/src")).start()
         }
         Thread.sleep(5000)
-        val benchmarkProcessList = (1..(threads / 2)).map {
-            val port = 8000 + it
-            ProcessBuilder("./redis-benchmark", "-p", port.toString()).directory(File("benchmark/redis-6.0.3/src")).start()
+        val benchmarkProcessList = (1..threads).map {
+            val port = startPort + it
+            val core = it * 2 - 1
+            val redisBenchmarkList = arrayListOf("./redis-benchmark", "-p", port.toString(), "--csv", "-r", "10000000", "-n", "1000000", "-d", "100")
+            val tasksetList = listOf("taskset", "-c", core.toString())
+            val cmd = if (isNuma) tasksetList + redisBenchmarkList else redisBenchmarkList
+            ProcessBuilder(cmd).directory(File("benchmark/redis/src")).start()
         }
         var counter = 1
         benchmarkProcessList.forEach {
@@ -28,8 +45,27 @@ object RedisBenchmark {
             val file = File("benchmark", "result_" + counter++)
             it.inputStream.copyTo(file.outputStream())
         }
+        println("Done")
 
         serverProcessList.forEach { it.waitFor() }
+    }
+
+    private fun prepare() {
+        print("Prepare... ")
+        val directory = File(dir)
+        if (!directory.exists()) {
+            directory.mkdir()
+        }
+        if (!File("$dir/$filename").exists()) {
+            val result = ProcessBuilder("wget", "http://download.redis.io/releases/$filename").directory(File(dir)).start().waitFor()
+            if (0 != result) {
+                throw Exception("Error download file $filename")
+            }
+        }
+        val unpackRedis = ProcessBuilder("tar", "-xf", filename).directory(File(dir)).start().waitFor()
+        File("$dir/redis-$version").renameTo(File("$dir/redis"))
+        val makeRedis = ProcessBuilder("make", "-j", cpuCount.toString()).directory(File("$dir/redis")).start().waitFor()
+        println("done")
     }
 
 }
